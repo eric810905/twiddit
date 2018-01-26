cb0005c47f0ff987d5eb0bc0d608f76e7d938081
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2018-01-26 17:35:07 +0000

    update streaming process(random select)

--- spark_streaming/stream_process.py
+++ spark_streaming/stream_process.py
@@ -7,12 +7,19 @@ from pyspark.streaming.kafka import KafkaUtils
 #    json parsing
 import json 
 import string
-
+from pyspark.sql import SparkSession
+import random
 
 sc = SparkContext(appName="spark_streaming_kafka")
 sc.setLogLevel("WARN")
 
-freq_vector_df = sc.read\
+spark = SparkSession(sc).builder\
+                .master("spark://10.0.0.5:7077")\
+                .appName("spark_stream")\
+                .config("--packages", "com.datastax.spark:spark-cassandra-connector_2.11:2.0.6,org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2")\
+                .getOrCreate()
+
+freq_vector_df = spark.read\
     .format("org.apache.spark.sql.cassandra")\
     .options(table="freq_vector", keyspace="playground")\
     .load()
@@ -21,13 +28,22 @@ others_df = spark.read\
     .format("org.apache.spark.sql.cassandra")\
     .options(table="others", keyspace="playground")\
     .load()
+
     
 subreddit_list = others_df.select("content").where("category = 'subreddits'").collect()
 subreddit_list = subreddit_list[0][0].split()
+
 total_count_list = others_df.select("content").where("category = 'total_counts'").collect()
 total_count_list = map(int, total_count_list[0][0].split())
 
-ssc = StreamingContext(sc, 10) 
+tweet_count_list = others_df.select("content").where("category = 'tweet_count'").collect()
+if len(tweet_count_list) == 0:
+    tweet_count_list = [ 0 for _ in range(len(subreddit_list)) ]
+    tweet_count_dict = dict(zip(subreddit_list, tweet_count_list ))
+else:
+    tweet_count_dict = json.loads(tweet_count_list[0][0])
+
+ssc = StreamingContext(sc, 60) 
 
 kafkaStream = KafkaUtils.createStream(ssc, 'ec2-35-161-255-24.us-west-2.compute.amazonaws.com:2181', 'spark-streaming', {'twitter':1}) 
 
@@ -40,15 +56,33 @@ def get_word_set(tweet_text):
     return set(tweet_text.split())
     
 def get_top_topic(word_set):
+    """
     query = "word = " + "' and word = '".join(word_set) + "'"
     word_count_list = freq_vector_df.select("counts").where(query).rdd.map(lambda row: map(int, row.counts.split())).collect()
     word_count_list = [sum(x) for x in zip(*word_count_list)]
     percentage_list = map(lambda x,y: x/float(y) if y != 0 else 1, word_count_list, total_count_list )
     return subreddit_list[ percentage_list.index(max(percentage_list)) ]
+    """
+    return random.sample(subreddit_list, 1)[0]
 
-parsed.map(lambda tweet: tweet['text']).pprint()
+#parsed.map(lambda tweet: get_word_set(tweet['text'])).pprint()
+
 subreddit_topic = parsed.map(lambda tweet: get_top_topic(get_word_set(tweet['text'])))
 subreddit_topic.pprint()
+
+def update_tweet_count(rdd):
+    for subreddit in rdd.collect():
+    	tweet_count_dict[subreddit] += 1
+
+subreddit_topic.foreachRDD(update_tweet_count)
+#for topic in subreddit_topic.collect():
+    #tweet_count_dict[topic] += 1
+
+spark.createDataFrame([{'category':'tweet_count_test', 'content': json.dumps(tweet_count_dict)}]).write\
+    .format("org.apache.spark.sql.cassandra")\
+    .mode('append')\
+    .options(table="others", keyspace="playground")\
+    .save()
 
 #parsed.map(lambda x: type(x)).pprint()
 #parsed.map(lambda x: filter ( lambda y: y in set(string.printable), x['text']) ).pprint()
