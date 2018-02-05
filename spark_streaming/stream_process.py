5cc20b0c35ec5fafcf9d70c81751d83483ebcda0
Ubuntu <ubuntu@ip-10-0-0-7.us-west-2.compute.internal>
2018-02-05 08:38:47 +0000

    finish documentation

--- spark_streaming/stream_process.py
+++ spark_streaming/stream_process.py
@@ -2,6 +2,13 @@
 This script streaming process the twitter messages from kafka. Each tweet is
 classified into a subreddit topic. The result is stored back to Cassandra.
 
+use the following command to run this file:
+spark-submit --conf spark.cassandra.connection.host=10.0.0.xyz \
+--packages com.datastax.spark:spark-cassandra-connector_2.11:2.0.6,\
+org.apache.spark:spark-streaming-kafka-0-8_2.11:2.0.2 \
+--executor-memory 6G --master spark://10.0.0.xyz:7077 \
+streaming_process.py
+
 """
 from pyspark import SparkContext  
 from pyspark.streaming import StreamingContext, StreamingListener
@@ -52,11 +59,14 @@ class myListener(StreamingListener):
         implements = ["org.apache.spark.streaming.api.java.PythonStreamingListener"]
 
 def get_tweet_count_dict(sparkContext):
-    """
+    """ create a dictionary of accumulator if the tweet_count_dict has not been
+    created. Set this dictionary as global variable
+    Returns: a global dictionary of accmulator which counts the number of
+      classified tweets in each subreddit
     """
     if ('tweet_count_dict' not in globals()):
         # Construct a dict of accumulators which counts the number of 
-        # classified tweets in each subreddt.
+        # classified tweets in each subreddit.
 	with open(os.path.dirname(__file__) + "/../config.json", "r") as f:
 	    config = json.load(f)
 
@@ -78,7 +88,8 @@ def get_tweet_count_dict(sparkContext):
     return globals()['tweet_count_dict']
 
 class twitterStreamingProcess( object ):
-    """
+    """ Streaming process the twitter messages. Classify each tweet to subreddit
+    topics and store the result in Cassandra.
     """
     def __init__( self ):
 	with open(os.path.dirname(__file__) + "/../config.json", "r") as f:
@@ -88,7 +99,10 @@ class twitterStreamingProcess( object ):
         self.subreddit_word_count_dict = self.get_subreddit_word_count()
 
     def get_word_set(self, tweet_text):
-        """ get a set of important words
+        """ Transform a string into a set of important words in this string.
+        Args:
+          text: string
+        Returns: set of important words
         """
 	tweet_text = filter ( lambda x: x in set(string.printable), tweet_text)
 	tweet_text = tweet_text.lower()
@@ -99,30 +113,43 @@ class twitterStreamingProcess( object ):
 	return set(important_word_list)
 
     def get_top_topic(self, word_set):
+        """ Classify a tweet to a subreddit topic given a set of words in the
+        tweet.
+        Args:
+          word_set: a set of word
+        Returns: A subreddit topic
+        """
 	if not word_set:
 	    return "No matched reddit"
-	word_freq_dict = defaultdict(float)
 	
+        word_freq_dict = defaultdict(float)
+	
+        # query cassandra to get the subreddits which have the words
         cluster = Cluster([self.config["DEFAULT"]["DBCLUSTER_PRIVATE_IP"]])
         session = cluster.connect(self.config["CASSANDRA"]["KEYSPACE"])
 	query = "SELECT counts FROM %s WHERE word IN ('" % (self.config["CASSANDRA"]["WORD_FREQUENCY_TABLE"]) + "', '".join(word_set) + "')"
 	response = session.execute(query)
 
+        # use tf or tf-itf to rank the subreddits
 	for row in response:
 	    freq_dict = json.loads(row.counts)
 	    for reddit in freq_dict:
                 # tf
 		#word_freq_dict[reddit] += freq_dict[reddit]
-	
                 # tf-itf
-		word_freq_dict[reddit] += (freq_dict[reddit] * log(self.num_subreddit/float(len(freq_dict))))
+		word_freq_dict[reddit] += (freq_dict[reddit] * \
+                    log(self.num_subreddit/float(len(freq_dict))))
 
 	session.shutdown()
+
 	if not word_freq_dict:
 	    return "No matched subreddit"
 	else:
-            for result, _ in Counter(word_freq_dict).most_common(self.config["TWITTER_STREAMING"]["SEARCH_TOP_SUBREDDIT_THRESHOULD"]):
-                if self.subreddit_word_count_dict[result] > self.config["TWITTER_STREAMING"]["WORD_COUNT_THRESHOULD"]:
+            for result, _ in Counter(word_freq_dict).most_common(self.config\
+                ["TWITTER_STREAMING"]["SEARCH_TOP_SUBREDDIT_THRESHOULD"]):
+                if self.subreddit_word_count_dict[result] > self.config\
+                    ["TWITTER_STREAMING"]["WORD_COUNT_THRESHOULD"]:
+	            tweet_count_dict[result].add(1)
                     return result
             return "No subreddit match the threshould constraints"
             """
@@ -132,9 +159,12 @@ class twitterStreamingProcess( object ):
             """
 
     def get_subreddit_word_count(self):
+        """ create a dictionary of total word count for each subreddit
+        """
         cluster = Cluster([self.config["DEFAULT"]["DBCLUSTER_PRIVATE_IP"]])
         session = cluster.connect(self.config["CASSANDRA"]["KEYSPACE"])
-	query = "SELECT * FROM %s" % (self.config["CASSANDRA"]["WORD_COUNT_TABLE"])
+	query = "SELECT * FROM %s" % \
+            (self.config["CASSANDRA"]["WORD_COUNT_TABLE"])
 	response = session.execute(query)
         
         subreddit_word_count_dict = {}
@@ -144,25 +174,29 @@ class twitterStreamingProcess( object ):
         return subreddit_word_count_dict
 
     def start(self):
-	"""
-	comsume messages from kafka and classify the tweets into topics
+	"""Comsume messages from kafka and classify the tweets into topics
 	"""
 	sc = SparkContext(appName="spark_streaming_kafka")
 	sc.setLogLevel("WARN")
-	ssc = StreamingContext(sc, self.config["TWITTER_STREAMING"]["MINI_BATCH_TIME_INTERVAL_SEC"] )
+	ssc = StreamingContext(sc, \
+            self.config["TWITTER_STREAMING"]["MINI_BATCH_TIME_INTERVAL_SEC"] )
 	listener = myListener(sc)
 	ssc.addStreamingListener(listener)
 
         self.num_subreddit = len(get_tweet_count_dict(sc))
-	print("==========classification to %d subreddits==========" % self.num_subreddit)
+	print("==========classification to %d subreddits==========" % \
+            self.num_subreddit)
 
-	kafkaStream = KafkaUtils.createStream(ssc, self.config["DEFAULT"]["KAFKA_PUBLIC_IP"]+':2181', 'spark-streaming', {'twitter':1})
+	kafkaStream = KafkaUtils.createStream(ssc, self.config["DEFAULT"]\
+            ["KAFKA_PUBLIC_IP"]+':2181', 'spark-streaming', {'twitter':1})
 	
 	# load streaming message from kafka
 	parsed = kafkaStream.map(lambda v: json.loads(v[1]))
 	parsed.count().map(lambda x:'Tweets in this batch: %s' % x).pprint()
 
-	subreddit_topic = parsed.map(lambda tweet: self.get_word_set(tweet['text']))
+        # process and classify tweets
+	subreddit_topic = \
+            parsed.map(lambda tweet: self.get_word_set(tweet['text']))
 	subreddit_topic.pprint()
 	subreddit_topic = subreddit_topic.map(self.get_top_topic)
 	subreddit_topic.pprint()
