28ae741b47142e3f47d108ed137024053130f8bb
Ubuntu <ubuntu@ip-10-0-0-6.us-west-2.compute.internal>
2018-02-06 17:49:07 +0000

    update kafka and web documnetations

--- web/app/views.py
+++ web/app/views.py
@@ -1,25 +1,28 @@
 # jsonify creates a json representation of the response
-from flask import jsonify
 from flask import render_template
 from app import app
-from flask import request
-
-# importing Cassandra modules from the driver we just installed
+import os.path
+import json
 from cassandra.cluster import Cluster
+from collections import Counter
 
-# Setting up connections to cassandra
+# load configuration
+with open(os.path.dirname(__file__) + "/../../config.json", "r") as f:
+    config = json.load(f)
 
-cluster = Cluster(['10.0.0.4'])
-session = cluster.connect('playground')
+cluster = Cluster([config["DEFAULT"]["DBCLUSTER_PRIVATE_IP"]])
+session = cluster.connect(config["CASSANDRA"]["KEYSPACE"])
 
+# demo web page
 @app.route("/demo")
 def demo():
  return render_template("bar2.html")
 
+# take GET request from bar2.html and return the data to display
 @app.route('/update', methods=['GET'])
 def update():
     query = "SELECT content FROM others WHERE category = 'tweet_count_web'"
     response = session.execute(query)
-    #return jsonify(response[0].content)
-    return response[0].content
+    return json.dumps(dict(Counter(json.loads(response[0].content)).most_common()[1:]))
+    #return response[0].content
 
